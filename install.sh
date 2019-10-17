f855dd4f7e3394e79f9e48b466db6efd3be1e8e6
yanorei32 <yanorei@hotmail.co.jp>
2019-10-17 11:40:13 +0900

    Add rofi config

--- install.sh
+++ install.sh
@@ -3,3 +3,8 @@
 # install vimrc
 ln -s $(cd $(dirname $0) && pwd)/vimrc ~/.vimrc
 
+# install rofi config
+mkdir -p ~/.config/rofi/
+ln -s $(cd $(dirname $0) && pwd)/rofi-config ~/.config/rofi/config
+
+
